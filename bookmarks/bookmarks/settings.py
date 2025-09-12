0d101857f35138498809c7fea8e040a78d3604b6
Mei-py <websocket.warrior@gmail.com>
2025-09-12 23:38:28 +0300

    добавлена аутентификация и логин для пользователей

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'acount.apps.AccountConfig',
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
