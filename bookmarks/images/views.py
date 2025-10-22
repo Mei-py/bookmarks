17bfb72dffa5287702e711ef70e832c0c3416826
Mei-py <websocket.warrior@gmail.com>
2025-10-22 21:39:57 +0300

    лайки и дизлайки, бесконечная пагинация

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -4,6 +4,10 @@ from django.contrib.auth.decorators import login_required
 from .forms import ImageCreatedForm
 from .models import Images
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 @login_required
@@ -46,3 +50,54 @@ def image_detail(request, id, slug):
             'image': image
         }
     )
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+
+    if image_id and action:
+        try:
+            image = Images.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Images.DoesNotExist:
+            pass
+        return JsonResponse({'status': 'error'})
+    
+
+@login_required
+def image_list(request):
+    images = Images.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+
+    if images_only:
+        return render(
+            request,
+            'images/image/list_images.html',
+            {'section': 'images', 'images': images}
+        )
+    
+    return render(
+        request,
+        'images/image/list.html',
+        {'section': 'images', 'images': images}
+    )
+
+    
